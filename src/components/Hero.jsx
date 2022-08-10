bf1a3520ae561e1ad9d114b152599315377d163c
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-10 12:39:07 +0000

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import image from '../assets/pic1.png'
+import v1 from '../assets/v1.svg'
+import v2 from '../assets/v2.svg'
 
 const Hero = () => {
   return (
 <div className="text-white body-font">
 <div className="  flex px-5 py-24 md:flex-row flex-col items-center">
-<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left  md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-white">Buld Your Awesome<br/>
-        <br className="hidden lg:inline-block"/>Platform
+<div className="lg:flex-grow md:w-1/2  flex flex-col md:items-start md:text-left  md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-white">Buld Your  Awesome<br/>
+        Platform
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom ech tumeric truffaut hexagon try-hard chambray.</p>
-      <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Our Services</button>
+      <p className="mb-8 leading-relaxed">Copper mug k pour-over freegan heirloom neutra air plant cold<br /> tacos poke beard tote bag. Heirloom ech tumeric truffaut hexagon try-hard chambray.</p>
+      <div className="flex justify-center  ">
+        <button className=" text-white bg-indigo-500 border-0 py-2 px-3 focus:outline-none hover:bg-indigo-600 rounded text-lg">Our Services</button>
     </div>
    </div>
-   <img src={image} className="object-cover object-center" alt="" />
-   </div>
-    {/** 
-
+    
+     <img src={image} alt="" className='relative' />
+     
+     <div className='absolute text-white flex top-[72%]  justify-between'>
+        <div className='pl-0'>
+        <img src={v1} alt="" className=''/>
+        </div>
+      
+      <div>
+      <img src={v2} alt="" className='left-0 ' />
       </div>
+      
+
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2  ">
-      <img className=" rounded w-[300px]" alt="hero" src={image}/>
-    </div>
-  </div>*/}
+    
+   </div>
+   
 </div>
 
   )
