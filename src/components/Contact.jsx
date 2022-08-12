1cddee76ea3108e14e4777e177124e0bcf1ff506
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-12 14:38:41 +0000

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
 <section class="text-white">
   <div class=" py-24 px-3 ">
     <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start ">
-      <h1 class="flex-grow sm:pr-16 text-5xl font-extrabold text-white items-center">Contact us for the service you want to use.</h1>
+      <h1 class="flex-grow sm:pr-16 text-5xl font-extrabold text-white items-center p-3">Contact us for the service you want to use.</h1>
       <button class="flex-shrink-0 text-white bg-blue-600 border-0 py-2 px-8  hover:bg-blue-700 rounded text-lg mt-10 flex sm:mt-0 justify-center">Contact us</button>
     </div>
   </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,13 @@ import fullvid from '../assets/fullimg.png'
 
 const Hero = () => {
   return (
-<div className="text-white body-font">
-<div className=" container flex px-5  md:flex-row flex-col items-center">
+<div className="text-white  ">
+<div className="  flex px-5  md:flex-row flex-col items-center">
 <div className="lg:flex-grow md:w-1/2  flex flex-col md:items-start md:text-left  md:mb-0 items-center text-center">
       <h1 className="  text-5xl mb-4 font-bold text-white">Build Your  Awesome<br/>
         Platform
       </h1>
-      <p className="mb-8 ">Copper mug k pour-over freegan heirloom neutra air plant cold<br /> tacos poke beard tote bag. Heirloom ech tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 ">AXES Technologies is  a digital fir that offers several services such as Web and App Development, Mentoring and Training  to Developers, we <br /> tacos poke beard tote bag. Heirloom ech tumeric truffaut hexagon try-hard chambray.</p>
       <div className="flex justify-center  ">
         <button className=" text-white bg-blue-600 border-0 py-2 px-3  hover:bg-blue-700 rounded text-lg">Our Services</button>
     </div>
@@ -25,8 +25,8 @@ const Hero = () => {
     <section class="text-white ">
   <div class=" py-24 ">
     <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start text-center">
-       <p class="flex-shrink-0 text-white  border-0 py-2 px-8 flex  font-bold text-4xl mt-10 sm:mt-0 justify-center mr-3">Why Axes Is  The Best Choice.</p>
-       <p class="pl-3 flex-shrink-0 text-white  border-0 py-2 px-8 flex   text-lg mt-10 sm:mt-0 justify-center">Lorem ipsum, dolor sit , labore fugit, enim eius culpa inventore vel aliquid, eveniet ullam quas nisi pariatur numquam earum ut dolor ipsam.</p>
+       <p class="flex-shrink-0 text-white  border-0 py-2 px-8 flex  font-bold text-4xl mt-10 sm:mt-0 justify-center mr-3">Why Axes Is  The Best Choice?</p>
+       <p class="pl-3 flex-shrink-0 text-white  border-0 py-2 px-8 flex   text-lg mt-10 sm:mt-0 justify-center">Lorem ipsum, dolor sit , labore fugit, em ut dolor ipsam.</p>
    
     </div>
   </div>
