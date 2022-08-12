08f683d4b182742f48815b1252974c47931beeb0
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-12 17:08:09 +0000

    Co-authored-by: Darkwa  <codewithdarkwa@users.noreply.github.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <h1 className="  text-5xl mb-4 font-bold text-white">Build Your  Awesome<br/>
         Platform
       </h1>
-      <p className="mb-8 ">AXES Technologies is  a digital fir that offers several services such as Web and App Development, Mentoring and Training  to Developers, we <br /> tacos poke beard tote bag. Heirloom ech tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 ">AXES Technologies is  a digital firm that offers several services such as Web and App Development, Mentoring and Training  to Developers, we <br /> tacos poke beard tote bag. Heirloom ech tumeric truffaut hexagon try-hard chambray.</p>
       <div className="flex justify-center  ">
         <button className=" text-white bg-blue-600 border-0 py-2 px-3  hover:bg-blue-700 rounded text-lg">Our Services</button>
     </div>
