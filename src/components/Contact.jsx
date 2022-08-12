601b0c91b2a890a30ce5a9fcd1ba93317a07164d
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-12 11:01:41 +0000

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   <div class=" py-24 px-3 ">
     <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start ">
       <h1 class="flex-grow sm:pr-16 text-5xl font-extrabold text-white items-center">Contact us for the service you want to use.</h1>
-      <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 flex sm:mt-0 justify-center">Contact us</button>
+      <button class="flex-shrink-0 text-white bg-blue-600 border-0 py-2 px-8  hover:bg-blue-700 rounded text-lg mt-10 flex sm:mt-0 justify-center">Contact us</button>
     </div>
   </div>
   <img src={v1} alt="" className='p-3'/>
